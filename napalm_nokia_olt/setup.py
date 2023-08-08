6909cc307dd5cd2742f4b0d699a016ad698cf79f
djohnnes <david.johnnes@gmail.com>
2023-08-08 18:29:49 +0100

    update the package to 0.0.30

--- napalm_nokia_olt/setup.py
+++ napalm_nokia_olt/setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="napalm_nokia_olt",
-    version="0.0.29",
+    version="0.0.30",
     author="Dave Macias",
     author_email = "davama@gmail.com",
     description=("Network Automation and Programmability Abstraction "
